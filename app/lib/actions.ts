16dbcf626e8b62167cd02299b18855e800fac4aa
neville Parmar <nevilleparmar@gmail.com>
2024-07-25 11:44:34 -0400

    charts and graphs on dashboard page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,7 +6,9 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation'; 
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
- 
+
+
+
 
 const FormSchema = z.object({
   id: z.string(),
@@ -25,21 +27,33 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+ 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
     await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-      `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
@@ -127,3 +141,4 @@ export async function authenticate(
     throw error;
   }
 }
+
